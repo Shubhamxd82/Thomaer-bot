05f11d662bdd4fe3bce51a085bb75e3cae908987
SUBHAJIT DAS <119496423+Shubhamxd82@users.noreply.github.com>
2026-04-18 20:58:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1328,3 +1328,20 @@ if __name__ == "__main__":
             time.sleep(1)
     except KeyboardInterrupt:
         logger.info("⚠️ Stopping bots...")
+
+
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_web).start()
